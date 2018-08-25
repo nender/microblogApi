dfc8ef2eebb9a5455c45a1c737faa2b18901f1b9
nender <nathan.ender@gmail.com>
2018-08-25 14:26:34 +0200

    Userscontroller now hides uneccesary information from returned json

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -6,42 +6,40 @@ using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 
 namespace microblogApi.Controllers {
+
+    [Produces("application/json")]
     [Route("/api/[controller]")]
-    public class UsersController : ControllerBase{
+    [ApiController]
+    public class UsersController : ControllerBase {
         readonly MicropostContext Db;
         readonly UserManager<User> UserManager;
-        public UsersController(MicropostContext context, UserManager<User> userManager)
-        {
+        public UsersController(MicropostContext context, UserManager<User> userManager) {
             Db = context;
             UserManager = userManager;
         }
 
         [HttpGet]
         public IActionResult GetUserIndex() {
-            return Ok(Db.Users);
+            return Ok(Db.Users.Select(x => x.ToViewModel()));
         }
 
         [HttpGet("{id}")]
         public IActionResult GetUser(long id) {
             var user = Db.Users.Find(id);
             if (user != null)
-                return Ok(user);
+                return Ok(user.ToViewModel());
             else
                 return NotFound();
         }
 
         [HttpPost]
-        public IActionResult Create([FromBody]PersonResponse person) {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
+        public IActionResult Create([FromBody]UserBindingModel person) {
             var user = new User { UserName = person.username, Email = person.email };
             var result = UserManager.CreateAsync(user, person.password).Result;
-            if (result.Succeeded) {
-                return Created("", user);
-            } else {
+            if (result.Succeeded)
+                return Created("", user.ToViewModel());
+            else
                 return BadRequest(result.Errors);
-            }
         }
 
         [HttpDelete("{id}")]
@@ -53,17 +51,35 @@ namespace microblogApi.Controllers {
             Db.Users.Remove(user);
             Db.SaveChanges();
 
-            return Ok(user);
+            return Ok(user.ToViewModel());
+        }
+    }
+
+    public static class UserExtensions {
+        public static UserViewModel ToViewModel(this User usr)
+            => new UserViewModel(usr);
+    }
+
+    public class UserViewModel {
+        readonly User _user;
+        public UserViewModel(User user) {
+            _user = user;
         }
 
-        public class PersonResponse
-        {
-            [Required]
-            public string username {get; set;}
-            [Required]
-            public string email {get; set;}
-            [Required]
-            public string password {get;set;}
-        }
+        public string username => _user.UserName;
+        public string email => _user.Email;
+        public long id => _user.Id;
+    }
+
+    public class UserBindingModel
+    {
+        [Required]
+        public string username { get; set; }
+
+        [Required]
+        public string email { get; set; }
+
+        [Required]
+        public string password { get; set; }
     }
 }
